809708ccf0b1452d9eea1f4ce6ba4dcceec330cc
GSowm <sowmya.kannan123@gmail.com>
2026-03-18 12:14:42 +0530

    task2

---
+++ Class_2/Datatyprd.js
@@ -0,0 +1,19 @@
+let brand_Name = "Apple";
+console.log(brand_Name);
+console.log(typeof(brand_Name));
+
+let price = 50000;
+let rating = 4.5;
+let delivery = 'Y';
+let discount = true
+let avail;
+console.log(price);
+console.log(typeof(price));
+console.log(rating);
+console.log(typeof(rating));
+console.log(delivery);
+console.log(typeof(delivery));
+console.log(discount);
+console.log(typeof(discount));
+console.log(avail);
+console.log(typeof(avail));
---
+++ Class_2/crud.js
@@ -0,0 +1,15 @@
+var eid = 101;
+console.log(eid);
+//document.writeln(eid);
+function login(params) {
+    console.log("Login Success");
+}
+class Account {
+    
+    }  
+    var emp = {
+        "eid": 101,
+        "ename": "Rahul",
+        "esalary": 50000
+    }
+    console.log(emp);
